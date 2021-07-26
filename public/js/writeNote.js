82831796ea1bdfdfc149ee8523540da7248e46b5
mposey301 <finalhq376@gmail.com>
2021-07-26 18:57:14 +0000

    Implemented tags

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -7,6 +7,8 @@ window.onload = () => {
             // this code runs if the user is logged in 
             console.log('logged in as', user.displayName);
             googleUser = user;
+            let message = document.getElementById("#message");
+            message.innerHTML = "What's on your mind " + googleUser.displayName;
         } else {
             // this code runs if the user not logged in 
             console.log('not logged in');
@@ -18,12 +20,15 @@ window.onload = () => {
         // get values from the form.
         const noteTitle = document.querySelector('#noteTitle').value;
         const noteText = document.querySelector('#noteText').value;
+        const today = new Date();
         console.log(noteTitle, noteText);
 
         // write these values to the database
         firebase.database().ref(`/users/${googleUser.uid}`).push({
              title: noteTitle,
-             text: noteText
+             text: noteText,
+             created: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(),
+             labels: []
         })
         .then(() => {
             console.log('Database write successful');
