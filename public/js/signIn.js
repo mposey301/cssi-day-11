258124bad474a5164c35c1ce5bfdba299ea734b4
mposey301 <finalhq376@gmail.com>
2021-07-26 17:43:30 +0000

    Redirect from index to writeNote

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,5 +1,15 @@
 const signInButton = document.querySelector('.button');
 signInButton.addEventListener('click', () => {
     console.log('clicked!');
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+
+        console.log('login succcess', user);
+        window.location = 'writeNote.html'
+    })
+    .catch(error => {
+        console.log('login failed', error);
+    });
 });
 
